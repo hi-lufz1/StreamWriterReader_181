97a04c28b9240dcfab0ef3a819abfa46eb0ed4ef
hi-lufz1 <latifufauzi@gmail.com>
2023-06-19 15:41:35 +0700

    14

--- exceptionstandard/exceptionstandard.cpp
+++ exceptionstandard/exceptionstandard.cpp
@@ -10,6 +10,12 @@ int main() {
 	cout << "Awal Program" << endl;// penanda 1
 	try {
 		array<int, 3> data = { 1,2,3 };
+		//pesan array interger 3 elemen
+		cout << data.at(4) << endl;
+		// memanggil array elemen ke 5
 
+	}
+	catch (exception& e) {
+		// penangkapan
 	}
 }
