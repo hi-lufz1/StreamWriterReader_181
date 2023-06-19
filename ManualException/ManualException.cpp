d5b02f8ca829281b74f6b1ff45471de87ae3774b
hi-lufz1 <latifufauzi@gmail.com>
2023-06-19 15:37:11 +0700

    11

--- ManualException/ManualException.cpp
+++ ManualException/ManualException.cpp
@@ -5,6 +5,22 @@ using namespace std;
 
 int main() {
 	try {
-		cout<<"Selamat Belajar di Prodi TI UMY"<< endl
+		cout << "Selamat Belajar di Prodi TI UMY" << endl;
+		throw 0.5; // melempar sebuah interger maka
+		cout << "Pernyataan tidak akan di eksekusi" << endl;
+
 	}
+
+	catch (int a) {
+		// blok ini akan dieksekusi
+		cout << "Pengecualian akan dieksekusi" << endl;
+
+	}
+	catch (...) {
+		//jika selain interger maka block ini akan dieksekusi
+		cout << "default pengecualian di eksekusi" << endl;
+
+	}
+	return 0;
+
 }
--- exceptionstandard/exceptionstandard.cpp
+++ exceptionstandard/exceptionstandard.cpp
@@ -1,4 +1,5 @@
 
 #include <iostream>
-
+#include <exception>
+//untuk obyek exception yang akan digunakan
 using namespace std;
